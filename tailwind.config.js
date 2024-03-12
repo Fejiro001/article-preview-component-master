58cceb346e726dd04438a7a421c4f126e68c1932
Abere Fejiro <aberefejiro@gmail.com>
2024-03-12 22:03:18 +0100

    some styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         "grayish-blue": "hsl(212, 23%, 69%)",
         "light-grayish-blue": "hsl(210, 46%, 95%)",
       },
+      fontFamily: {
+        primary: ["Manrope","sans-serif"]
+      },
+      fontSize: {
+        base: "clamp(1rem, 1.45vw, 3rem)",
+        paragraph: "clamp(0.813rem, .45vw, 2rem)"
+      }
     },
   },
   plugins: [],
